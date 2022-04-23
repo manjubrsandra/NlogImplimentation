1443544232aab64abfddd1d05dfcdffb3f07b9a3
manjubrsandra <manjunathbrsandra43@gmail.com>
2022-04-23 16:42:03 +0530

    Add project files.

---
+++ NlogImplimentation/Addnum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlogImplimentation
+{
+     class Addnum
+    {
+        NLog nLog = new NLog();
+        public int Sum(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                nLog.LogDebug("Debug Successfull : Sum()");
+                nLog.LogError("Expecting null values");
+                nLog.LogWarn("a or b should not equal to 0");
+            }
+            int c = a + b;
+            nLog.LogDebug("Debug Successfull : Sum()");
+            nLog.LogInfo("Sum method passed, Result" + c);
+            return c;
+
+        }
+    }
+}
---
+++ NlogImplimentation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlogImplimentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD");
+            Addnum obj = new Addnum();
+            obj.Sum(2, 3);
+            Console.WriteLine("This is a Logger Demo");
+            Console.ReadKey();
+        }
+    }
+}
